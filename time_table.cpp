ca6e889338a6c2220d6956206bd6481056b01905
Looags <91559774+GhostJets@users.noreply.github.com>
2021-10-13 13:32:17 -0400

    Question 2

---
+++ time_table.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int multiple;
+  cout<<"Enter a number from 1-12\n";
+  cin>>multiple;
+  for(int g = 1; g <=12; g++){
+    cout<<multiple*g<<'\n';
+  }
+}
