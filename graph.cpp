430227e9a390c76342f71c0308acfadfa27b1996
Looags <91559774+GhostJets@users.noreply.github.com>
2021-10-14 11:56:25 -0400

    Graph Machine

---
+++ graph.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a;
+  for (int k = 0; k<5; k++){
+        cout<<"\n"<<"Enter your number between 1 and 50 ";
+    cin>>a;
+    for (int n = 0; n < a; n++){
+
+    cout<<"#";
+    }
+  }
+  }
+
