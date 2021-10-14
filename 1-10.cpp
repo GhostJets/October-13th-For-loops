a701d3c8e81e196e0db65298be560953e358cf19
Looags <91559774+GhostJets@users.noreply.github.com>
2021-10-14 12:07:56 -0400

    Question 3

---
+++ 1-10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+      int a;
+  for (int k = 0; k<=10; k++){
+    cout<<"\n";
+    for (int n = 0; n < k; n++){
+  
+    cout<<"*";
+    
+    }
+  }
+}
